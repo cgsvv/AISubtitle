cc91db238d21e79704c8dd5e02f4bcb6d3d2f44b
cgsv <cgsv@cgsvdeMacBook-Pro.local>
2023-03-03 01:21:58 +0800

    move to ts

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
+            <code className={styles.code}>pages/index.tsx</code>
           </p>
           <div>
             <a
