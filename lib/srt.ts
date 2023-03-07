cc1a78b14d0c8f0c42ac0ed9f1d8aa0b20a3c15e
cgsv <cgsv@cgsvdeMacBook-Pro.local>
2023-03-07 23:07:28 +0800

    add srt page

--- lib/srt.ts
+++ lib/srt.ts
@@ -1,10 +1,16 @@
 import languageEncoding from "detect-file-encoding-and-language";
-import * as fs from "fs";
+// import * as fs from "fs";
 
 export type Node = {
     pos: string;
     timestamp?: string;
     content: string;
+}
+
+export function checkIsSrtFile(content: string) {
+    const p1 = content.indexOf("\n");
+    const r = /^\s*(\d+:\d+:\d+,\d+)[^\S\n]+-->[^\S\n]+(\d+:\d+:\d+,\d+)/;
+    return r.test(content.slice(p1+1, 50));
 }
 
 export async function getEncoding(src: any) {
@@ -26,7 +32,9 @@ function block_to_node(block: string, linesep: string): Node | undefined {
     }
 }
 
-export function parseSrt(text: string, linesep="\r\n") {
+export function parseSrt(text: string) {
+    text = text.replaceAll("\r", "");
+    const linesep = "\n";
     const blocks = text.split(linesep+linesep);
     const nodes: Node[] = [];
     for (const block of blocks) {
@@ -44,20 +52,20 @@ function node_to_srt_text(node: Node, linesep: string): string {
     return jlst.join(linesep);
 }
 
-export function nodesToSrtText(nodes: Node[], linesep="\r\n"): string {
+export function nodesToSrtText(nodes: Node[], linesep="\n"): string {
     return nodes.map(node => node_to_srt_text(node, linesep)).join(linesep+linesep);
 }
 
-export function nodesToTransNodes(nodes: Node[], trans: string[], append=false, linesep="\r\n"): Node[] {
+export function nodesToTransNodes(nodes: Node[], trans: string[], append=false, linesep="\n"): Node[] {
     return nodes.map((node, idx) => {
         const oldContent = append ? node.content + linesep: "";
         return {...node, content: oldContent + (trans[idx] || "")};
     });
 }
 
-export async function readTextFile(src: string) {
-    const encoding = await getEncoding(src);
-    const data = fs.readFileSync(src);
-    const s = new TextDecoder(encoding!).decode(data);
-    return s;
-}
+// export async function readTextFile(src: string) {
+//     const encoding = await getEncoding(src);
+//     const data = fs.readFileSync(src);
+//     const s = new TextDecoder(encoding!).decode(data);
+//     return s;
+// }
---
+++ pages/srt.tsx
@@ -0,0 +1,131 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { useState } from 'react';
+import { getEncoding, parseSrt, Node, nodesToSrtText, checkIsSrtFile, nodesToTransNodes } from '@/lib/srt';
+
+const MAX_FILE_SIZE = 512 * 1024; // 512KB
+const PAGE_SIZE = 15;
+const MAX_RETRY = 5;
+
+function curPageNodes(nodes: Node[], curPage: number) {
+    return nodes.slice(curPage*PAGE_SIZE, (curPage+1)*PAGE_SIZE);
+}
+
+function curPageNodesText(nodes: Node[], curPage: number) {
+    const curNodes = curPageNodes(nodes, curPage);
+    return nodesToSrtText(curNodes);
+}
+
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
+async function traslate_all(nodes: Node[]) {
+    const batches: Node[][] = [];
+    for (let i = 0; i < nodes.length; i+=PAGE_SIZE) {
+        batches.push(nodes.slice(i, i+PAGE_SIZE));
+    }
+    // for now, just use sequential execution
+    const results: Node[] = [];
+    for (const batch of batches) {
+        let success = false;
+        for (let i = 0; i < MAX_RETRY && !success; i++) {
+            try {
+                const r = await translate_one_batch(batch);
+                results.push(...r);
+                success = true;
+            } catch (e) {
+                console.error(e);
+                await sleep(3000);   // may exceed rate limit, sleep for a while
+            }
+        }
+        if (!success) {
+            console.error(`translate_all failed for ${batch}`);
+        }
+    }
+    return results;
+}
+
+async function translate_one_batch(nodes: Node[]) {
+    let options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify( {
+            "targetLang":"中文",
+            "sentences": nodes.map(node => node.content)
+        })
+    };
+
+    const res = await fetch('/api/translate', options);
+    const jres: string[] = await res.json();
+    return nodesToTransNodes(nodes, jres);
+}
+
+function clearFileInput() {
+    const finput = document.getElementById("file") as HTMLInputElement;
+    if (finput) {
+        finput.value = "";
+    }
+}
+
+export default function Srt() {
+    const [srt, setSrt] = useState("");
+    const [nodes, setNodes] = useState<Node[]>([]);
+    const [curPage, setCurPage] = useState(0);
+
+    const onChooseFile = async (e) => {
+        const input = e.target;
+        const f: File = input.files[0];
+        if (!f) return;
+        if (f.size > MAX_FILE_SIZE) {
+            alert("Max file size 512KB");
+            clearFileInput();
+        }
+        const encoding = await getEncoding(f);
+        const data = await f.arrayBuffer();
+        const text = new TextDecoder(encoding!).decode(data);
+        if (!checkIsSrtFile(text)) {
+            alert("Not a valid SRT file");
+            clearFileInput();
+        }
+        const nodes = parseSrt(text);
+        setNodes(nodes);
+        setCurPage(0); 
+    };
+
+    const toPage = (delta: number) => {
+        const newPage = curPage + delta;
+        if (newPage < 0 || newPage >= nodes.length / PAGE_SIZE) return;
+        setCurPage(newPage);
+    }
+
+    const translate = async () => {
+        const newnodes = await translate_one_batch(curPageNodes(nodes, curPage));
+        setSrt(nodesToSrtText(newnodes));
+    }
+
+    return (
+        <>
+        <Head>
+            <title>srt converter</title>
+        </Head>
+        <main>
+            <div style={{display: "flex"}}>
+            <div style={{display: "flex", height: "100vh", width: "400px", flexDirection: "column", alignItems: "center", justifyContent: "space-evenly"}}>
+                <input onChange={onChooseFile} type="file" id="file" style={{display: "block", margin: "30px"}} />
+                <div>
+                    <button onClick={()=>toPage(-1)} type="button" style={{margin:"20px", width: "60px", height: "30px"}}>prev</button>
+                    <p style={{display: "inline"}}>{curPage+1} / {Math.ceil(nodes.length / PAGE_SIZE)}</p>
+                    <button onClick={() => toPage(1)} type="button" style={{margin:"20px", width: "60px", height: "30px"}}>next</button>
+                </div>
+                <textarea value={curPageNodesText(nodes, curPage)} readOnly style={{width:"375px", height: "600px"}}></textarea>
+            </div>
+            <div style={{width: "400px"}}>
+                <button onClick={translate} type="button" style={{margin:"20px", width: "60px", height: "30px"}}>translate</button>
+                <textarea value={srt} readOnly style={{marginTop:"108px", width:"375px", height: "600px"}}></textarea>
+            </div>
+            </div>
+        </main>
+        </>
+    )
+}
