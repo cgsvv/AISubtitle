0435e83a590c963821f342b1f4345b0371b6a00e
cgsv <cgsv@cgsvdeMacBook-Pro.local>
2023-03-09 03:14:05 +0800

    add translation icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,12 +2,18 @@ import Image from "next/image";
 import Github from "./Github";
 import { useLocalStorage } from "react-use";
 import { checkOpenaiApiKey } from "@/lib/openai/openai";
-import { toast, Toaster } from "react-hot-toast";
+import { toast } from "react-hot-toast";
 import { useTranslation } from 'next-i18next';
 
 export default function Header() {
     const [userKey, setUserKey] = useLocalStorage<string>("user-openai-apikey-trans");
     const {t} = useTranslation("common");
+    const { i18n } = useTranslation();
+
+    const changeLang = () => {
+        const newLang = i18n.language === "en"? "zh-CN" : "en";
+        i18n.changeLanguage(newLang);
+    }
 
     const setOpenAIKey = () => {
         const key = prompt(t("Request-Key")!);
@@ -25,6 +31,7 @@ export default function Header() {
                 <Github width="33" height="33"></Github>
             </div>
             <div style={{marginRight: "10px", marginTop: "10px"}}>
+                <Image onClick={changeLang} style={{marginRight: "20px"}} alt="settings" width={33} height={33} src="/trans.png" />
                 <Image onClick={setOpenAIKey} alt="settings" width={33} height={33} src="/set1.png" />
             </div>
         </div>
